114d85e6f931dc457a5c5edeb33f1ef8eebeb0c3
Bruce Phillips <phillips1021@hotmail.com>
2013-10-23 15:23:47 -0500

    Added getting collection of Person objects and exposing that collection to the view

--- springmvcwebexample/src/main/java/edu/ku/it/si/springmvcwebexample/web/HomeController.java
+++ springmvcwebexample/src/main/java/edu/ku/it/si/springmvcwebexample/web/HomeController.java
@@ -1,8 +1,15 @@
 package edu.ku.it.si.springmvcwebexample.web;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import edu.ku.it.si.springmvcwebexample.model.Person;
+import edu.ku.it.si.springmvcwebexample.service.PersonService;
 
 @Controller
 /**
@@ -14,11 +21,29 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping(value="/")
 public class HomeController {
 	
+	
+	private PersonService personService;
+	
 	@RequestMapping(method=RequestMethod.GET)
-	  public String home() {
+	  public String home(Model model) {
+		
+		List<Person> people = personService.getPeople();
+		
+		model.addAttribute("people", people);
 		
 	    return "home";
 	    
 	  }
+
+	/**
+	 * @param personService the personService to set
+	 */
+	@Autowired
+	public void setPersonService(PersonService personService) {
+		this.personService = personService;
+	}
+	
+	
+	
 
 }
